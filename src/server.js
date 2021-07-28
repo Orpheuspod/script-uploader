889987b782caaf24cdd64638570c1406c61e5b9f
warren yun <xxwarrenhdxx@gmail.com>
2021-07-28 16:31:51 -0400

    Create basic server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Started server on port ${port}`));
