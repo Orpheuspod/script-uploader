7b6e132adf41f98c2ce430c207346bb524469d2f
warren yun <xxwarrenhdxx@gmail.com>
2021-07-28 17:24:09 -0400

    Add transcript router to server

--- src/server.js
+++ src/server.js
@@ -5,4 +5,9 @@ const app = express();
 app.use(cors);
 
 const port = process.env.PORT || 4000;
+
+const transcriptRouter = require('../routes/transcript');
+
+app.use('/transcript', transcriptRouter);
+
 app.listen(port, () => console.log(`Started server on port ${port}`));
