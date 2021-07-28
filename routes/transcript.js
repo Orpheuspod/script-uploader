fe372086c8c1091ed28b0e1e307948f597189291
warren yun <xxwarrenhdxx@gmail.com>
2021-07-28 17:17:22 -0400

    Basic structure for transcript route

--- routes/transcript.js
+++ routes/transcript.js
@@ -1,2 +1,11 @@
 const router = require('express').Router();
 
+router.route('/get').get((req, res) => {
+    // github api stuff
+});
+
+router.route('/upload').post((req, res) => {
+    // github api stuff
+});
+
+module.exports = router;
